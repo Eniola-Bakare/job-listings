46fb206f3d094a38c54cce98eb883359cdb15266
Eniola Bakare <eebakare@gmail.com>
2023-04-13 13:53:54 +0100

    fully functional! I am actually very glad!

--- script.js
+++ script.js
@@ -11,24 +11,33 @@ const jobListWrapper = document.querySelector('.job-list-wrapper')
 
 let filteredArr = []
 let selectedRoles = []
+let typeArr = []
 
 // filter func
 const filterFunc = function(nodeItem, type){
   nodeItem.addEventListener('click', function(){
-    console.log(type, 'parentEl')
-    searchbar.classList.remove('hidden'); 
-    selectedRoles.push(nodeItem?.textContent)
-    console.log(nodeItem?.textContent, 'nodeItemTextCOntent')
+    // console.log(type, 'parentEl')
+    searchbar.classList.remove('hidden');
+    
+    console.log(selectedRoles.includes(nodeItem.textContent), 'checking include first')
+    
+    // the !selectedRoles.includes... checks that the item is not present, while the type checks to see that the type was not selected too
+    if(!selectedRoles.includes(nodeItem.textContent)){
+      selectedRoles.push(nodeItem?.textContent)
+      typeArr.push(type) //lang/role etc i should remove this sha
+      
+      searchElWrapper.insertAdjacentHTML('afterbegin', `
+          <div class="search-el">
+            <p class="search-name">${nodeItem?.textContent}</p>
+            <button class="close-btn"><img src="./images/icon-remove.svg" alt="close button"/></button>
+          </div>
+      `)
+    }
+    // console.log(nodeItem?.textContent, 'nodeItemTextCOntent')
     // console.log(eachRole.parentElement.parentElement, 'node parent item')
     // console.log(eachRole, 'node item')
     
     // add it to the search bar --- step 1
-    searchElWrapper.insertAdjacentHTML('afterbegin', `
-        <div class="search-el">
-          <p class="search-name">${nodeItem?.textContent}</p>
-          <button class="close-btn"><img src="./images/icon-remove.svg" alt="close button"/></button>
-        </div>
-    `)
 
     // for close button n the search elements in the search bar
     let closeBtnNode = [...document.querySelectorAll('.close-btn')]
@@ -42,42 +51,79 @@ const filterFunc = function(nodeItem, type){
             return updateUi(data)
           }
       })
-    console.log(closeBtnNode.length, 'length node<')
-    console.log(closeBtnNode.length, 'length after cls btn btn')
+    // console.log(closeBtnNode.length, 'length node<')
+    // console.log(closeBtnNode.length, 'length after cls btn btn')
+    console.log(selectedRoles, 'selectedRoles', selectedRoles.length)
+    console.log(selectedRoles.includes(nodeItem.textContent), 'checking include after')
     })
 
 
-    console.log( 'node item class list' , nodeItem.classList, type)
-    console.log(selectedRoles, 'selected roles')
-
+    // console.log( 'node item class list' , nodeItem.classList, type)
+    // console.log(selectedRoles, 'selected roles')
+    
+    const presentSelectedItems = []
     const filterText = document.querySelector('.search-name').textContent.toLowerCase();
-    // console.log(filterText, 'filter search text')
+    console.log(filterText, 'filter search text')
+    console.log(selectedRoles, 'selected search text')
     filteredArr = data.filter(eachRequiredJob => {
 
       const typeCheck = Array.isArray(eachRequiredJob[type])
-      console.log(typeCheck, 'typecheck')
-      if(typeCheck){
+      // console.log(typeCheck, 'typecheck')
+      
+      const eachDataValueArr =[]
+
+      if (selectedRoles.length > 1) {
+
+        for (const [key, value] of Object.entries(eachRequiredJob)) {
+          if (Array.isArray(value)) {
+            value.map(eachI => eachDataValueArr.push(eachI.toLowerCase()))
+          }else  if (typeof value === 'string'){
+            eachDataValueArr.push(value.toLowerCase())            
+          }
+        }
+
+        console.log(eachRequiredJob.company, 'company name')
+        // console.log(eachDataValueArr, 'company name')
+        // console.log(eachDataValueArr)
+
+        let checking = selectedRoles.every(eachSelection => {
+          return eachDataValueArr.includes(eachSelection.toLowerCase())
+           })
+           if (checking) {
+            console.log(eachRequiredJob)
+            presentSelectedItems.push(eachRequiredJob)
+            return eachRequiredJob
+           }
+          //  console.log(checking)
+          //  console.log(presentSelectedItems)
+          //  console.log(includes)
+          //  console.log( includes)
+      // console.log(presentSelectedItems, 'length')
+        // return presentSelectedItems
+      } 
+      else if(typeCheck){
           const eachArr = eachRequiredJob[type].filter( eachItem => {
             return filterText === eachItem.toLowerCase()
           })
-          // if(eachArr){ console.log(eachRequiredJob)}
+          // if(eachArr.){ console.log(eachRequiredJob)}
           if (eachArr.length >= 1) return eachRequiredJob
 
-        }
-      else if(!typeCheck) {
+        } else if(!typeCheck) {
         const eachRole = eachRequiredJob[type].toLowerCase()
-        console.log(eachRole)
+        console.log(eachRole === filterText)
         return eachRole === filterText
         // console.log('Not an array')
       }
-
+      console.log(presentSelectedItems)
       // console.log(typeof eachRequiredJob[type] === 'object')
       // //
       // console.log(eachRole)
       // return eachRole === filterText
     })
-    console.log(filteredArr)
-    console.log(filteredArr)
+
+
+    // console.log(filteredArr)
+    // console.log(filteredArr)
     jobListWrapper.innerHTML =''
     return updateUi ([...filteredArr])
       
@@ -121,10 +167,6 @@ const updateUi = function(dataItem){
       `)
   })
 
-  const funcC = function(){
-    console.log('position here')
-  }
-    
   document.querySelectorAll('.role').forEach((eachRole, _, parent) => filterFunc(eachRole, 'role'))
   document.querySelectorAll('.languages').forEach(eachlang => filterFunc(eachlang, 'languages'))
   document.querySelectorAll('.level').forEach(eachPosition => filterFunc(eachPosition, 'level')) 
@@ -138,10 +180,10 @@ clearBtn.addEventListener('click', function(){
   selectedRoles = []
   filteredArr = []
   jobListWrapper.innerHTML = searchElWrapper.innerHTML = ''
-  console.log(filteredArr)
-  console.log(searchbar.classList)
-  console.log(selectedRoles, 'selected roles')
-  console.log(document.querySelectorAll('.role') )
+  // console.log(filteredArr)
+  // console.log(searchbar.classList)
+  // console.log(selectedRoles, 'selected roles')
+  // console.log(document.querySelectorAll('.role') )
   updateUi([...data])
 })
 
